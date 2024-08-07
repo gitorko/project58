735c18ae67b2d7d617391b61ae0e4dff9fc79001
Arjun Surendra <asurendra@vmware.com>
2024-08-07 16:33:04 +0530

    code

---
+++ src/test/java/com/demo/project58/MainTest.java
@@ -0,0 +1,17 @@
+package com.demo.project58;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MainTest extends BaseTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
