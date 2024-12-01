238806d2d1b984f9e6ee0dbfe5775e6d5c054a07
Arjun Surendra <asurendra@vmware.com>
2024-12-01 23:26:40 +0530

    code

--- src/test/java/com/demo/project58/controller/FullContextControllerTest.java
+++ src/test/java/com/demo/project58/controller/FullContextControllerTest.java
@@ -21,10 +21,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestClient;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
 @AutoConfigureMockMvc
 @Import(TestContainerConfiguration.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
